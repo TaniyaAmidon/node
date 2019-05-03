86936d2061558c506396ca43f7aaeebc7f5319bf
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-03 17:24:46 +0100

    read file module

--- node_practice.js
+++ node_practice.js
@@ -1,9 +1,14 @@
 const http = require('http');
 const dt = require('./myfirstmodule')
+const fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write(data);
+    res.end();
+  });
+
 }).listen(8080);
 
