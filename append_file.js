92ca13c3bb3e9ff30c63618fa2386907bb9d45f6
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-03 17:47:25 +0100

    update and delete methods

--- append_file.js
+++ append_file.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
 
+//append conents to a file if file doesn't exit new file will be created
 fs.appendFile('newfile1.text', 'hello world', function(err) {
   if (err) throw err;
   console.log('saved');
 });
 
+
+//depending on the flag argument file will be open to 'w' = write
 fs.open('newfile12.text', 'w', function(err){
   if (err) throw err;
   console.log('open');
+});
+
+//will override a existing file, if the file doesn't exist new file will be created
+fs.writeFile('newfile1.text', 'hiii', function(err) {
+  if (err) throw err;
+  console.log('rewrite!');
+});
+
+//delete file
+fs.unlink('newfile12.text', function(err) {
+  if (err) throw err;
+  console.log('delete!');
 });
